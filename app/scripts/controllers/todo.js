e6f217c4e2dbc994568d39e155ca370817c8dacd
Tate Rathje <taterathje@gmail.com>
2017-05-23 02:02:38 -0700

    set everything up for webpack

--- app/scripts/controllers/todo.js
+++ app/scripts/controllers/todo.js
@@ -1,18 +1,20 @@
 'use strict';
 
-angular.module('todoListApp')
-.controller('todoCtrl', function($scope, dataService) {
+function TodoCtrl($scope, dataService) {
   $scope.deleteTodo = function(todo, index) {
     $scope.todos.splice(index, 1);
     dataService.deleteTodo(todo);
   };
-  
+
   $scope.saveTodos = function() {
-    var filteredTodos = $scope.todos.filter(function(todo){
-      if(todo.edited) {
+    var filteredTodos = $scope.todos.filter(function(todo) {
+      if (todo.edited) {
         return todo
       };
     })
     dataService.saveTodos(filteredTodos);
-  }; 
-});
+  };
+
+}
+
+module.exports = TodoCtrl;
